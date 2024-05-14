5e8ec33f3dcf7dc555acc0e030f77f9c4e98607e
tapendu-technoexponent98 <tapendu@technoexponent.com>
2024-05-14 23:07:14 +0530

    first addition,subtraction , multiplication and division app using kotlin

--- app/src/main/java/com/example/firstandroid/MainActivity.kt
+++ app/src/main/java/com/example/firstandroid/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.example.firstandroid
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMul : Button
+    lateinit var btnDiv : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var resultTV : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_sub)
+        btnMul = findViewById(R.id.btn_mul)
+        btnDiv = findViewById(R.id.btn_div)
+        etA = findViewById(R.id.et_1)
+        etB = findViewById(R.id.et_2)
+        resultTV = findViewById(R.id.result_tv)
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMul.setOnClickListener(this)
+        btnDiv.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        var a = etA.text.toString().toDouble()
+        var b = etB.text.toString().toDouble()
+        var result = 0.0
+        if (p0 != null) {
+            when(p0?.id){
+                R.id.btn_add -> {
+                    result =  a+b
+                }
+
+                R.id.btn_sub -> {
+                    result = a-b
+                }
+                R.id.btn_mul -> {
+                    result = a*b
+                }
+                R.id.btn_div -> {
+                    result = a/b
+                }
+            }
         }
+        resultTV.text = "Result is $result"
     }
 }
